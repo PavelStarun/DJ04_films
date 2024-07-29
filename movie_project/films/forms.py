31d3a258db2dae98875b7c61146275290ee33870
PavelStarun <starunpavelpython@gmail.com>
2024-07-29 21:47:43 +0300

    готово

--- movie_project/films/forms.py
+++ movie_project/films/forms.py
@@ -12,3 +12,10 @@ class MovieForm(forms.ModelForm):
             'rating': 'Оценка',
             'link': 'Ссылка'
         }
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'review': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control'}),
+            'link': forms.URLInput(attrs={'class': 'form-control'}),
+        }
